b9560155227a52ee0d1ab4d0564b3e4d3eda43d6
halitov-r-k <halitov.r.k@gmail.com>
2022-06-16 07:12:22 +0500

    2022-06-16T07:10

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,59 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Main method" );
+        Connection connection;
+        Statement statement;
+        //открытие соединения
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/db",
+                    "root",
+                    "root"
+                    );
+            statement = connection.createStatement();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        //проверка соединения
+        try {
+            if(!connection.isClosed()) { System.out.printf("Connect is open %b \n", true);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //работа с db
+        try {
+            //вставка записи
+            // statement.execute("INSERT INTO table1(Id, Data) VALUES(1, 'date1');");
+            //обновление записи
+            int result = statement.executeUpdate("update table1 set Data='NewData1' where Id=1");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //закрытие соединения
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //проверка закрытия соединения
+        try {
+            if(connection.isClosed()) { System.out.printf("Connect is close %b \n", true);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
